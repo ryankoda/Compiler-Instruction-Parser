f3e2fe2f3f13df0ab095cde4305b96ae1c9585c5
RYAN KODA <kodadota@gmail.com>
2019-01-03 14:32:16 -0800

    Update buffer.cpp

--- buffer.cpp
+++ buffer.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 bool InputBuffer::EndOfInput()
 {
+	//If the input file is empty, return EOF
 	if (!input_buffer.empty())
 		return false;
 	else
@@ -18,6 +19,7 @@ bool InputBuffer::EndOfInput()
 
 char InputBuffer::UngetChar(char c)
 {
+	//If it's not the EOF, place the buffer back one space
 	if (c != EOF)
 		input_buffer.push_back(c);;
 	return c;
@@ -25,6 +27,7 @@ char InputBuffer::UngetChar(char c)
 
 void InputBuffer::GetChar(char& c)
 {
+	//Parse the next character
 	if (!input_buffer.empty()) {
 		c = input_buffer.back();
 		input_buffer.pop_back();
@@ -36,6 +39,7 @@ void InputBuffer::GetChar(char& c)
 
 string InputBuffer::UngetString(string s)
 {
+	//Place the buffer before the read in string
 	for (int i = 0; i < s.size(); i++)
 		input_buffer.push_back(s[s.size() - i - 1]);
 	return s;
