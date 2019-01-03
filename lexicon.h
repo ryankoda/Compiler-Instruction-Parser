604a81b5082a18d5f6534519d88ac81ce24838b6
RYAN KODA <kodadota@gmail.com>
2019-01-03 14:38:46 -0800

    Update lexicon.h

--- lexicon.h
+++ lexicon.h
@@ -1,1 +1,43 @@
 #pragma once
+#include <vector>
+#include <string>
+
+#include "buffer.h"
+
+typedef enum {
+	END_OF_FILE = 0,
+	VAR, IF, WHILE, SWITCH, CASE, DEFAULT, PRINT, ARRAY, FOR,
+	PLUS, MINUS, DIV, MULT,
+	EQUAL, COLON, COMMA, SEMICOLON,
+	LBRAC, RBRAC, LPAREN, RPAREN, LBRACE, RBRACE,
+	NOTEQUAL, GREATER, LESS,
+	NUM, ID, ERROR
+} TokenType;
+
+class Token {
+public:
+	void Print();
+
+	std::string lexeme;
+	TokenType token_type;
+	int line_no;
+};
+
+class LexicalAnalyzer {
+public:
+	Token GetToken();
+	TokenType UngetToken(Token);
+	LexicalAnalyzer();
+
+private:
+	std::vector<Token> tokens;
+	int line_no;
+	Token tmp;
+	InputBuffer input;
+
+	bool SkipSpace();
+	bool IsKeyword(std::string);
+	TokenType FindKeywordIndex(std::string);
+	Token ScanIdOrKeyword();
+	Token ScanNumber();
+};
