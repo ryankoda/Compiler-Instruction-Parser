0b043a6f9cf5b852a5782297f0bb8a8ed7209dc3
RYAN KODA <kodadota@gmail.com>
2019-01-03 14:20:49 -0800

    Update buffer.h

--- buffer.h
+++ buffer.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <string>
+class InputBuffer {
+public:
+	void GetChar(char&);
+	char UngetChar(char);
+	std::string UngetString(std::string);
+	bool EndOfInput();
+
+private:
+	std::vector<char> input_buffer;
+};
